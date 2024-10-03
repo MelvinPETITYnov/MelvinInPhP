b8bee4005d16f43d0ade291dca2eb8b67f7fe646
mpetit <melvin.petit@ynov.com>
2024-10-03 11:53:54 +0200

    problem with my style, didn't link with my index.php

--- app/index.php
+++ app/index.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>acceuil</title>
+    <link rel="stylesheet" href="index.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-    <?php 
 
-    ?>
+<header>
+    <h1>WELCOME TO MY CV</h1>
+</header>
 
+<div class="container">
+    <p>Voici un exemple de page PHP classique. Vous pouvez modifier ce texte et ajouter plus de contenu.</p>
+</div>
 </body>
 </html>
